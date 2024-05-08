595b2f0391194beca0f41d1191cc1ea0afd98032
Makar Chupriyanov <makarchupriyanov@gmail.com>
2024-05-08 16:28:39 +0500

    Добавьте файлы проекта.

---
+++ ShapeAreaLibrary/Shapes/Circle.cs
@@ -0,0 +1,14 @@
+﻿using ShapeAreaLibrary.Interfaces;
+
+namespace ShapeAreaLibrary.Shapes
+{
+    public class Circle : IShape
+    {
+        private double radius;
+        public double GetArea() => Math.PI * radius * radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+    }
+}
---
+++ ShapeAreaLibrary/Shapes/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShapeAreaLibrary.Interfaces;
+
+namespace ShapeAreaLibrary.Shapes
+{
+    public class Triangle : IShape
+    {
+        private double sideA, sideB, sideC;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+        }
+
+        public double GetArea()
+        {
+            double p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+
+        public bool IsRight()
+        {
+            double max = Math.Max(sideA, Math.Max(sideB, sideC));
+            double min = Math.Min(sideA, Math.Min(sideB, sideC));
+            double middle = sideA + sideB + sideC - max - min;
+
+            return max * max == min * min + middle * middle;
+        }
+    }
+}
